2994dd270b3b7d83d9667a09988e871ae5c729ee
ljm208 <98561794+ljm208@users.noreply.github.com>
2022-02-17 20:09:30 -0500

    roll-a-ball
    finished

--- Assets/Scenes/scripts/PlayerControls.cs
+++ Assets/Scenes/scripts/PlayerControls.cs
@@ -2,21 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 public class PlayerControls : MonoBehaviour
 {
-    
-    private Rigidbody rb;
+    // Create public variables for player speed, and for the Text UI game objects
+    public float speed;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
+
     private float movementX;
     private float movementY;
-    
-    // Start is called before the first frame update
+
+    private Rigidbody rb;
+    private int count;
+
+    // At the start of the game..
     void Start()
     {
+        // Assign the Rigidbody component to our private rb variable
         rb = GetComponent<Rigidbody>();
+
+        // Set the count to zero 
+        count = 0;
+
+        SetCountText();
+
+        // Set the text property of the Win Text UI to an empty string, making the 'You Win' (game over message) blank
+        winTextObject.SetActive(false);
     }
 
-    void OnMove(InputValue movementValue)
+    private void OnMove(InputValue movementValue)
     {
         Vector2 movementVector = movementValue.Get<Vector2>();
 
@@ -24,10 +40,35 @@ public class PlayerControls : MonoBehaviour
         movementY = movementVector.y;
     }
 
-    void FixedUpdate()
+    private void FixedUpdate()
     {
         Vector3 movement = new Vector3(movementX, 0.0f, movementY);
-        
-        rb.AddForce(movement);
+
+        rb.AddForce(movement * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("stationary"))
+        {
+            other.gameObject.SetActive(false);
+
+            // Add one to the score variable 'count'
+            count = count + 1;
+
+            // Run the 'SetCountText()' function (see below)
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+
+        if (count >= 6)
+        {
+            // Set the text value of your 'winText'
+            winTextObject.SetActive(true);
+        }
     }
 }
